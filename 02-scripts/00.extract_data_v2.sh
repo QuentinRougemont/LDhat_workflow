a622c75ea18063aa2135ca7232be90e6fb42d011
quentin <quentinrougemont@orange.fr>
2019-12-04 10:29:13 +0100

    add script

--- 02-scripts/00.extract_data_v2.sh
+++ 02-scripts/00.extract_data_v2.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
 #input file = vcftools splitted by chromosome and by population
-module load vcftools 
-
-if [ $# -ne 3 ]; then
-    echo "USAGE: $0 vcffile popname chr_list"
+#the name of the pop should appears in the vcf name 
+#the name of the chromosome should then appears in second
+if [ $# -ne 2 ]; then
+    echo "USAGE: $0 popname chr_list"
     echo "Expecting the following values on the command line, in that order"
-    echo "name of the vcf"
-    echo "name of the population"
+    echo "name of the population" #could be replaced by name of the vcf instead
     echo "list of all wanted chromosomes"
 fi
 
-vcffile=$1
-if [ -z "$vcffile" ]
-then
-    echo "############"
-    echo "STOP : I need a vcffile to work"
-    echo exit
-fi
+pop=$1
 
-pop=$2
 if [ -z "$pop" ]
 then
     echo "############"
@@ -27,7 +19,7 @@ then
     echo exit
 fi
 
-list_chromo=$3
+list_chromo=$2
 if [ -z "$list_chromo" ]
 then
     echo "############"
@@ -37,10 +29,10 @@ fi
 
 for i in $(cat "$list_chromo" ) ; do 
     mkdir -p "$pop"/chromo."$i"                      
-    vcftools --vcf ${vcffile} \
+    vcftools --vcf 00-data/batch."$pop".recode.vcf \
         --chr "$i" \
         --recode --recode-INFO-all \
-        --out "$pop"/chromo."$i"/batch."$pop"."$i"   
+        --out "$pop"/chromo."$i"/batch."$pop"."$i"
 
     vcftools --vcf  "$pop"/chromo."$i"/batch."$pop"."$i".recode.vcf \
     --maf 0.05 \
@@ -57,14 +49,15 @@ do
     overlap=500 # $3
     grep -v "#" "$input" |cut -f 1-2 > list_position
 
-    python3 ../../02-scripts/01.split_dataset.py \
-	list_position \
-	"$size" \
-	"$overlap"
+   ../../02-scripts/01.split_dataset.py list_position "$size" "$overlap"
 
     for j in dataset_* ; 
     do
-        vcftools --vcf "$input" --positions $j --ldhat-geno --chr $i --out batch_1.$j 
+        vcftools --vcf "$input" \
+        --positions $j \
+        --ldhat-geno \
+        --chr $i \
+        --out batch_1.$j 
     done
     cd ../../
 done
