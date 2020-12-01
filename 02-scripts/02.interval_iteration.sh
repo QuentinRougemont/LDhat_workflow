d2c6497739d2dc8fa3b68611ae05933b0db69fce
quentin <quentinrougemont@orange.fr>
2020-12-01 12:15:00 +0100

    removed some useless lines

--- 02-scripts/02.interval_iteration.sh
+++ 02-scripts/02.interval_iteration.sh
@@ -2,33 +2,27 @@
 
 #Global var
 id=$[ $1  ]
-#dataset=$(ls -d batch_1.dataset*./)
-#data=${dataset[$id]}
-
-#set color for log
-RED='\033[0;31m'
-NC='\033[0m'
 
 #Interval options 
 iter=30000000
 samp=5000
  
-echo -e ${RED}##################################################################${NC}
-echo  "	     	  running $dataset with  $iter iterations"
-echo  "	     	  sampling every $samp iterations"
-echo -e ${RED}##################################################################${NC}
+echo  "       running $dataset with  $iter iterations"
+echo  "       sampling every $samp iterations"
+echo "##################################################################"
 
 cd batch_1.dataset."$id"/ 
 
 interval -seq batch_1.dataset_"$id".ldhat.sites \
-	 -loc batch_1.dataset_"$id".ldhat.locs \
-	 -lk ../../../new_lk.txt  \
-	 -its "$iter" \
-	 -bpen 5 \
-	 -samp "$samp"
+     -loc batch_1.dataset_"$id".ldhat.locs \
+    -lk ../../../new_lk.txt  \
+    -its "$iter" \
+    -bpen 5 \
+    -samp "$samp"
 
-echo -e ${RED}##################################################################${NC}
-echo -e "	summarising final Ne r estimates --\n\tburning 100 first iterations "
-echo -e ${RED}##################################################################${NC}
+echo "##################################################################"
+echo -e "   summarising final Ne r estimates --\n\tburning 100 first iterations "
+echo  "##################################################################"
 
 stat -input rates.txt -burn 250 -loc batch_1.dataset_"$id".ldhat.locs
+#/home/quentin/programs/LDhat/stat -input rates.txt -burn 250 -loc batch_1.dataset_"$id".ldhat.locs
--- 02-scripts/03.rhomap_iteration.sh
+++ 02-scripts/03.rhomap_iteration.sh
@@ -3,34 +3,20 @@
 #Global var
 id=$[ $1  ]
 
-#set color for log
-RED='\033[0;31m'
-NC='\033[0m'
-
 #Rhomap options 
 iter=40000000
 samp=5000
 bpen=2
 burn=1000000
 
-echo -e ${RED}##################################################################${NC}
-echo  "	     	  running $dataset with  $iter iterations"
-echo  "	     	  sampling every $samp iterations"
-echo -e ${RED}##################################################################${NC}
-
 cd batch_1.dataset."$id"/ 
 
 rhomap -seq batch_1.dataset_"$id".ldhat.sites \
-	 -loc batch_1.dataset_"$id".ldhat.locs \
-	 -lk ../../../new_lk.txt  \
-	 -burn "$burn" \
+    -loc batch_1.dataset_"$id".ldhat.locs \
+    -lk ../../../new_lk.txt  \
+    -burn "$burn" \
      -its  "$iter" \
-	 -bpen "$bpen" \
-	 -samp "$samp" \
+    -bpen "$bpen" \
+    -samp "$samp" \
      -prefix rhomap_
 
-echo -e ${RED}##################################################################${NC}
-echo -e "	summarising final Ne r estimates --\n\tburning 100 first iterations "
-echo -e ${RED}##################################################################${NC}
-
-#stat -input rates.txt -burn 250 -loc batch_1.dataset_"$id".ldhat.locs
